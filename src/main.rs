d870cabd5b286d86ce2d85c0d0e059aec832f4a7
Mohammed <a.a.mxhamed@gmail.com>
2026-07-25 00:53:42 +0100

    added l7 support

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,52 @@
+use bytes::BytesMut;
+use serde::Deserialize;
 use std::{
-    sync::{Arc, atomic::{AtomicBool, AtomicUsize, Ordering}, Mutex},
     net::SocketAddr,
+    sync::{
+        Arc, Mutex,
+        atomic::{AtomicBool, AtomicUsize, Ordering},
+    }, 
     time::Duration,
 };
 use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt}, 
+    io::{AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
     sync::RwLock,
 };
-use serde::Deserialize;
-use bytes::BytesMut;
 
 #[derive(Debug, Deserialize)]
 struct Config {
-    backends: Vec<ServerConfig>,
-    listen: ServerConfig,
+    mode: String,
+    backends: Vec<BackendServerConfig>,
+    listen: ListenServerConfig,
     buffer_pool_size: Option<usize>,
     buffer_size: Option<usize>,
 }
 
 #[derive(Debug, Deserialize)]
-struct ServerConfig {
+struct ListenServerConfig {
     address: String,
     port: u16,
 }
 
+#[derive(Debug, Clone, Deserialize)]
+struct BackendServerConfig {
+    name: Option<String>,
+    address: String,
+    port: u16,
+    path: Option<String>,
+    #[serde(default = "default_weight")]
+    weight: usize,
+}
+
+fn default_weight() -> usize {
+    1
+}
+
 #[derive(Debug, Clone)]
 struct Backend {
-    addr: SocketAddr,
-    healthy: Arc<AtomicBool>, 
+    server: BackendServerConfig,
+    healthy: Arc<AtomicBool>,
 }
 
 type BackendPool = Arc<RwLock<Vec<Backend>>>;
@@ -39,33 +57,45 @@ async fn main() {
     let config_content = tokio::fs::read_to_string("config.yaml").await.unwrap();
     let config: Config = serde_yaml::from_str(&config_content).unwrap();
 
-    let listen_addr = load_listen_addr(&config);
+    let listen_addr = get_addr_from_config(&config.listen.address, config.listen.port);
     let listener = TcpListener::bind(listen_addr).await.unwrap();
     println!("Server listening on {}", listen_addr);
 
     let backends_pool: BackendPool = Arc::new(RwLock::new(load_backends(&config)));
-    let counter = Arc::new(AtomicUsize::new(0));
     let count = config.buffer_pool_size.unwrap_or(20);
     let buf_size = config.buffer_size.unwrap_or(8192);
-    let buffers_pool: BufferPool = Arc::new(Mutex::new((0..count).map(|_| BytesMut::with_capacity(buf_size)).collect()));
+    let counter = Arc::new(AtomicUsize::new(0));
+    let buffers_pool: BufferPool = Arc::new(Mutex::new(
+        (0..count)
+            .map(|_| BytesMut::with_capacity(buf_size))
+            .collect(),
+    ));
 
     spawn_health_checker(backends_pool.clone());
 
     loop {
-        let (socket, addr) = listener.accept().await.unwrap();
+        let (mut socket, addr) = listener.accept().await.unwrap();
         println!("Accepted connection from {}", addr);
         let counter = counter.clone();
         let backends_pool = backends_pool.clone();
         let buffers_pool = buffers_pool.clone();
+        let mode = config.mode.clone();
         tokio::spawn(async move {
-            let backend = pickbackend(backends_pool, counter).await;
-            if let Some(backend) = backend {
+            let backends = get_valid_backends(&mode, backends_pool, &mut socket).await;
+            if let Some(backends) = backends {
+                let backend = pick_backend(backends, counter.clone());
+                let addr = get_addr_from_config(&backend.server.address, backend.server.port);
                 let (buf1, buf2) = {
                     let mut pool = buffers_pool.lock().unwrap();
-                    (pool.pop().unwrap_or_else(|| BytesMut::with_capacity(buf_size)),
-                    pool.pop().unwrap_or_else(|| BytesMut::with_capacity(buf_size)))
+                    (
+                        pool.pop()
+                            .unwrap_or_else(|| BytesMut::with_capacity(buf_size)),
+                        pool.pop()
+                            .unwrap_or_else(|| BytesMut::with_capacity(buf_size)),
+                    )
                 };
-                let (processed_buf1, processed_buf2) = process(socket, backend.addr, buf1, buf2).await;
+                let (processed_buf1, processed_buf2) =
+                    process(socket, addr, buf1, buf2).await;
                 {
                     let mut pool = buffers_pool.lock().unwrap();
                     pool.push(processed_buf1);
@@ -78,18 +108,21 @@ async fn main() {
     }
 }
 
-fn load_listen_addr(config: &Config) -> SocketAddr {
-    format!("{}:{}", config.listen.address, config.listen.port).parse().unwrap()
+fn get_addr_from_config(address: &str, port: u16) -> SocketAddr {
+    format!("{}:{}", address, port)
+        .parse()
+        .unwrap()
 }
 
 fn load_backends(config: &Config) -> Vec<Backend> {
-    config.backends.iter().map(|server| {
-        let addr = format!("{}:{}", server.address, server.port).parse().unwrap();
-        Backend {
-            addr,
-            healthy: Arc::new(AtomicBool::new(true)), // Assume healthy initially
-        }
-    }).collect()
+    config
+        .backends
+        .iter()
+        .map(|backend| Backend {
+            server: backend.clone(),
+            healthy: Arc::new(AtomicBool::new(true)),
+        })
+        .collect()
 }
 
 fn spawn_health_checker(pool: BackendPool) {
@@ -97,7 +130,8 @@ fn spawn_health_checker(pool: BackendPool) {
         loop {
             let backends = pool.read().await.clone();
             for backend in backends {
-                let healthy = check_health(backend.addr).await;
+                let addr = get_addr_from_config(&backend.server.address, backend.server.port);
+                let healthy = check_health(addr).await;
                 backend.healthy.store(healthy, Ordering::Relaxed);
             }
             tokio::time::sleep(Duration::from_secs(5)).await;
@@ -106,26 +140,98 @@ fn spawn_health_checker(pool: BackendPool) {
 }
 
 async fn check_health(addr: SocketAddr) -> bool {
-    match tokio::time::timeout(
-        Duration::from_millis(500),
-        TcpStream::connect(addr)
-    ).await {
+    match tokio::time::timeout(Duration::from_millis(500), TcpStream::connect(addr)).await {
         Ok(Ok(_)) => true,
         _ => false,
     }
 }
 
-async fn pickbackend(backends_pool: Arc<RwLock<Vec<Backend>>>, counter: Arc<AtomicUsize>) -> Option<Backend> {
+async fn get_valid_backends(mode: &str, backends_pool: Arc<RwLock<Vec<Backend>>>, socket: &mut TcpStream) -> Option<Vec<Backend>> {
     let backends = backends_pool.read().await;
-    let healthy_backends: Vec<_> = backends.iter().filter(|b| b.healthy.load(Ordering::Relaxed)).collect();
+    let healthy_backends: Vec<_> = backends
+        .iter()
+        .cloned()
+        .filter(|b| b.healthy.load(Ordering::Relaxed))
+        .collect();
+
     if healthy_backends.is_empty() {
         return None;
     }
-    let counter_value = counter.fetch_add(1, Ordering::Relaxed) % healthy_backends.len();
-    Some(healthy_backends[counter_value].clone())
+
+    if mode == "l4" {
+        return Some(healthy_backends);
+    }
+    
+    let mut buffer = Vec::new();
+    let mut valid_backends: Vec<Backend> = Vec::new();
+    loop {
+        let n = socket.read_buf(&mut buffer).await.unwrap();
+
+        if n == 0 {
+            break;
+        }
+
+        let mut headers = [httparse::EMPTY_HEADER; 32];
+        let mut req = httparse::Request::new(&mut headers);
+
+        match req.parse(&buffer).unwrap() {
+            httparse::Status::Complete(body_start) => {
+                // Headers parsed
+
+                // Read Content-Length if present
+                let content_length = req
+                    .headers
+                    .iter()
+                    .find(|h| h.name.eq_ignore_ascii_case("Content-Length"))
+                    .and_then(|h| std::str::from_utf8(h.value).ok())
+                    .and_then(|s| s.parse::<usize>().ok())
+                    .unwrap_or(0);
+
+                // Wait until the whole body is available
+                if buffer.len() < body_start + content_length {
+                    continue;
+                }
+
+                // let body = &buffer[body_start..body_start + content_length];
+
+                let path = req.path.unwrap_or("/");
+                println!("Received request for path: {}", path);
+                
+                for backend in &healthy_backends {
+                    if path.starts_with(&backend.server.path.as_deref().unwrap_or("/")) {
+                        valid_backends.push(backend.clone());
+                    }
+                }
+
+                break;
+            }
+
+            httparse::Status::Partial => continue,
+        }
+    }
+    Some(valid_backends)
 }
 
-async fn process(mut socket: TcpStream, addr: SocketAddr, mut buf1: BytesMut, mut buf2: BytesMut) -> (BytesMut, BytesMut) {
+fn pick_backend(backends: Vec<Backend>, counter: Arc<AtomicUsize>) -> Backend {
+    let total_weight: usize = backends.iter().map(|b| b.server.weight).sum();
+    let index = counter.fetch_add(1, Ordering::Relaxed) % total_weight;
+
+    let mut cumulative = 0;
+    for backend in &backends {
+        cumulative += backend.server.weight;
+        if index < cumulative {
+            return backend.clone();
+        }
+    }
+    backends[0].clone()
+}
+
+async fn process(
+    mut socket: TcpStream,
+    addr: SocketAddr,
+    mut buf1: BytesMut,
+    mut buf2: BytesMut,
+) -> (BytesMut, BytesMut) {
     println!("Forwarding connection to {}", addr);
     let mut upstream = match TcpStream::connect(addr).await {
         Ok(stream) => stream,
@@ -134,7 +240,7 @@ async fn process(mut socket: TcpStream, addr: SocketAddr, mut buf1: BytesMut, mu
             return (buf1, buf2);
         }
     };
-    
+
     loop {
         buf1.clear();
         buf2.clear();
